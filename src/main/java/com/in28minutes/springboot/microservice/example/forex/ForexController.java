d547853fe0e639b9841e33c1e73ab5aec088d2a6
Yaqin Chen Hedin <Yaqin.Chen.Hedin@hiq.se>
2020-02-26 16:25:30 +0100

    my second commit to local repository

--- src/main/java/com/in28minutes/springboot/microservice/example/forex/ForexController.java
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/ForexController.java
@@ -38,6 +38,6 @@ public class ForexController {
     @RequestMapping("/")
     public String index() {
 
-        return "Greetings from Spring Boot!----";
+        return "Greetings from Spring Boot!----with github connected";
     }
 }
